8aa79922cba7f51141b469ae86afb78fe934ff2c
Zerlz <asker_nokia@live.con>
2021-05-16 14:18:33 +0800

    fix bug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 
 from monitor import NamebaseMonitor
 from sawalonwatchdog import DogManager
-
+from dd_notification import NotificationManager
 if __name__ == '__main__':
     logging.basicConfig(filename='./logs/trader_monitor.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                         level=logging.DEBUG)
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     dog.start_watch()
 
     monitor.get_hns_price()
-    monitor.dingmessage('HNS 小助手上班啦！')
+    NotificationManager().message_normal('HNS 小助手上班啦！')
 
     schedule.every(10).seconds.do(monitor.get_hns_price)
-    schedule.every().hour.do(monitor.triger_log_price)
+    schedule.every().hour.do(monitor.trigger_log_price)
     try:
         while True:
             schedule.run_pending()
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         dog.stop()
         dog.join()
         logging.info('Exception Graceful shutdown of Baleen ingestion service.')
-        monitor.dd_message_at('HNS 小助手下线啦！\n Graceful shutdown of Baleen ingestion service.')
+        NotificationManager().message_at_all('HNS 小助手下线啦！\n Graceful shutdown of Baleen ingestion service.')
     except Exception as e:
         logging.info('Exception %s' % (str(e)))
-        monitor.dd_message_at('HNS 小助手下线啦！\n %s' % (str(e)))
+        NotificationManager().message_at_all('HNS 小助手下线啦！\n %s' % (str(e)))
